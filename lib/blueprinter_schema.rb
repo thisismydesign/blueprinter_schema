97a3b678ccafabd98df534a4afc47bf538d5c369
Csaba Apagyi <csaba.apagyi@gmail.com>
2025-05-14 19:28:40 +0000

    Rubocop

--- lib/blueprinter_schema.rb
+++ lib/blueprinter_schema.rb
@@ -4,7 +4,11 @@ require_relative 'blueprinter_schema/version'
 require_relative 'blueprinter_schema/generator'
 
 module BlueprinterSchema
-  def self.generate(serializer, model, options = { include_conditional_fields: true, fallback_type: {}, view: :default })
+  def self.generate(
+    serializer,
+    model,
+    options = { include_conditional_fields: true, fallback_type: {}, view: :default }
+  )
     Generator.new(serializer, model, options).generate
   end
 end
